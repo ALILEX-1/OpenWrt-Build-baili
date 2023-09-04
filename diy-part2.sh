b30b7e48dd569030d35675f26e569db8fd7b26ce
tashifool <61131342+tashifool@users.noreply.github.com>
2023-09-04 13:37:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,8 @@
 
 # 设置默认IP为 192.168.2.1
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 修改内核为5.15.127-1-f12cad7e28a8d370c8c790636360fd31
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/.127/.127-1/g' include/kernel-5.15
+sed -i 's/add0a575341b263a06e93599fc220a5dd34cb4ca5b9d05097a5db2a061928f26/f12cad7e28a8d370c8c790636360fd31/g' include/kernel-5.15
